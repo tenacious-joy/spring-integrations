f991148f4527214eb34a1e022393c1212b77aa2c
Aswini <Aswini.J@nationstarmail.com>
2018-04-07 14:01:03 +0530

    Spring-integrations-channel bootstrapping

---
+++ src/main/java/com/example/springintegration/channels/springintegrationdemo/SpringIntegrationDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springintegration.channels.springintegrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationDemoApplication.class, args);
+	}
+}
