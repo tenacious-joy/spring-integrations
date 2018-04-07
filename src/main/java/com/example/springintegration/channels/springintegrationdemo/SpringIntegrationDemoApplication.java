fe8fa373ed7a5c757fdb90b9a11bf87a89b2fc6e
Aswini <Aswini.J@nationstarmail.com>
2018-04-07 15:31:13 +0530

    producer,consumer configurations with poller

--- src/main/java/com/example/springintegration/channels/springintegrationdemo/SpringIntegrationDemoApplication.java
+++ src/main/java/com/example/springintegration/channels/springintegrationdemo/SpringIntegrationDemoApplication.java
@@ -1,12 +1,14 @@
 package com.example.springintegration.channels.springintegrationdemo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
 public class SpringIntegrationDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringIntegrationDemoApplication.class, args);
+
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"/META-INF/spring/si-components.xml");
+		// SpringApplication.run(SpringIntegrationDemoApplication.class, args);
 	}
 }
